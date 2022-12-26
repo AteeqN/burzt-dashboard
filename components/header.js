f6a41a370fed4a1b546ab64c627f495dc2aa96c9
Ateeq Anwar <igz.mgt.02@gmail.com>
2022-12-26 12:08:55 +0500

    link change of slack sign in

--- components/header.js
+++ components/header.js
@@ -135,8 +135,8 @@ function Header({ data }) {
               ) : (
                 <>
                   <Nav.Link
-                    // href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fburzt.passwise.app%2Fsign-in&client_id=2214326541360.4420644231987"
-                    href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2F3b4f-206-84-190-35.in.ngrok.io%2Fsign-in&client_id=2214326541360.4420644231987"
+                    href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fburzt.passwise.app%2Fsign-in&client_id=2214326541360.4420644231987"
+                    // href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2F2c30-206-84-188-47.eu.ngrok.io%2Fsign-in&client_id=2214326541360.4420644231987"
                     className={customHeader.button}
                   >
                     <svg
