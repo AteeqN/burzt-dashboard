dff47abf1afbf0186b47ba29bb15fac935982690
Ateeq Anwar <igz.mgt.02@gmail.com>
2023-01-06 19:03:55 +0500

    ng

--- components/header.js
+++ components/header.js
@@ -112,41 +112,8 @@ function Header() {
               <Nav.Link href="/about" className={customHeader.customLink}>
                 About Us
               </Nav.Link> */}
-              {!token ? (
+              {token ? (
                 <>
-                  <Nav.Link
-                    href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fburzt.passwise.app%2Fsign-in&client_id=2214326541360.4420644231987"
-                    // href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fa23f-206-84-189-29.in.ngrok.io%2Fsign-in&client_id=3608320528308.4328768473665"
-                    className={customHeader.button}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className={customHeader.svg}
-                      viewBox="0 0 122.8 122.8"
-                    >
-                      <path
-                        d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
-                        fill="#E01E5A"
-                      ></path>
-                      <path
-                        d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
-                        fill="#36C5F0"
-                      ></path>
-                      <path
-                        d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
-                        fill="#2EB67D"
-                      ></path>
-                      <path
-                        d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
-                        fill="#ECB22E"
-                      ></path>
-                    </svg>
-                    Sign in with Slack
-                  </Nav.Link>
-                </>
-              ) : (
-                <>
-                  {/* {!loading ? <Loader /> : null} */}
                   <NavDropdown title="Settings" id="basic-nav-dropdown">
                     <NavDropdown.Item href="profile">
                       {/* <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style="width: 150px;"
@@ -173,7 +140,36 @@ function Header() {
                     </NavDropdown.Item>
                   </NavDropdown>
                 </>
-              )}
+              ) : null}
+              <Nav.Link
+                href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fburzt.passwise.app%2Fsign-in&client_id=2214326541360.4420644231987"
+                // href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fa23f-206-84-189-29.in.ngrok.io%2Fsign-in&client_id=3608320528308.4328768473665"
+                className={customHeader.button}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className={customHeader.svg}
+                  viewBox="0 0 122.8 122.8"
+                >
+                  <path
+                    d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
+                    fill="#E01E5A"
+                  ></path>
+                  <path
+                    d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
+                    fill="#36C5F0"
+                  ></path>
+                  <path
+                    d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
+                    fill="#2EB67D"
+                  ></path>
+                  <path
+                    d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
+                    fill="#ECB22E"
+                  ></path>
+                </svg>
+                Sign in with Slack
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
